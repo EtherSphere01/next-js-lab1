e17ece39a2cc97744ffe81f811aec8572bb6598d
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-05-07 16:40:23 +0600

    first

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import LandingPage from "./LandingPage/page";
+import LandingPage2 from "./LandingPage2/page";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <LandingPage></LandingPage>
         {children}
       </body>
     </html>
